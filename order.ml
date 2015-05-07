a34820941c4e74178db8b030cd031c00e16520a4
Benjamin <benjamin.farinier@ens-lyon.fr>
2015-05-07 11:56:54 +0200

    fix type

--- order.ml
+++ order.ml
@@ -1,84 +1,95 @@
 open MapBoard
 
+type utag = [`Armies|`Fleets]
+type otag = [`Hold|`Move|`SupportA|`SupportD|`Convoy]
+
 type order =
   | AHold of armies units
   | AMove of armies units * armies province
-  | ASupportD of armies units * armies units
-  | ASupportA of armies units * armies units * armies province
+  | ASupportD of armies units * armies province
+  | ASupportA of armies units * armies province * armies province
   | FHold of fleets units
   | FMove of fleets units * fleets province
-  | FSupportD of fleets units * fleets units
-  | FSupportA of fleets units * fleets units * fleets province
+  | FSupportD of fleets units * fleets province
+  | FSupportA of fleets units * fleets province * fleets province
   | FConvoy of fleets units * armies province * armies province
+
+
+let error_province = "incompatible province type"
+let error_units = "incompatible unit type"
+let error_number = "wrong number of argument"
+
 
 let utoa (u: any units) : armies units =
   match specialize u with
   | Some u, None -> u
-  | _ -> raise (Invalid_argument "units_to_armies")
+  | _ -> raise (Invalid_argument error_units)
 let utof (u: any units) : fleets units =
   match specialize u with
   | None, Some u -> u
-  | _ -> raise (Invalid_argument "units_to_fleets")
+  | _ -> raise (Invalid_argument error_units)
 
 let ptoa (p: any province) : armies province =
   match from_any p with
   | Some p, None, None -> inland_to_armies p
   | None, None, Some p -> coastal_to_armies p
-  | _ -> raise (Invalid_argument "province_to_armies")
+  | _ -> raise (Invalid_argument error_province)
 
 let ptof (p: any province) : fleets province =
   match from_any p with
   | None, Some p, None -> water_to_fleets p
   | None, None, Some p -> coastal_to_fleets p
-  | _ -> raise (Invalid_argument "province_to_fleets")
+  | _ -> raise (Invalid_argument error_province)
 
-let search str t =
+
+let search (str: string) (t: t) =
   try search_by_name str t with
     Not_found -> search_by_abbr str t
 
-let list1 t = function
+let list1 (t: t) = function
     [a] -> search a t
-  | _ -> raise (Invalid_argument "list1")
+  | _ -> raise (Invalid_argument error_number)
 let list2 t = function
     [a;b] -> (search a t), (search b t)
-  | _ -> raise (Invalid_argument "list2")
+  | _ -> raise (Invalid_argument error_number)
 let list3 t = function
     [a;b;c] -> (search a t), (search b t), (search c t)
-  | _ -> raise (Invalid_argument "list2")
+  | _ -> raise (Invalid_argument error_number)
 
-let make u t (b: t) (lst: string list) =
+
+let make (u: [< utag]) (o: [< otag]) (t: t) (lst: string list) =
   match u with
   | `Armies -> begin
-      match t with
+      match o with
       | `Hold ->
-        let p1 = list1 b lst in
-        AHold (utoa (on_province b p1))
+        let p1 = list1 t lst in
+        AHold (utoa (on_province t p1))
       | `Move ->
-        let (p1,p2) = list2 b lst in
-        AMove (utoa (on_province b p1), ptoa p2)
+        let (p1,p2) = list2 t lst in
+        AMove (utoa (on_province t p1), ptoa p2)
       | `SupportD ->
-        let (p1,p2) = list2 b lst in
-        ASupportD (utoa (on_province b p1), utoa (on_province b p2))
+        let (p1,p2) = list2 t lst in
+        ASupportD (utoa (on_province t p1), ptoa p2)
       | `SupportA ->
-        let (p1,p2,p3) = list3 b lst in
-        ASupportA (utoa (on_province b p1), utoa (on_province b p2), ptoa p3)
-      | `Convoy -> raise (Invalid_argument "make_order")
+        let (p1,p2,p3) = list3 t lst in
+        ASupportA (utoa (on_province t p1), ptoa p2, ptoa p3)
+      | `Convoy -> raise (Invalid_argument error_units)
     end
   | `Fleets -> begin
-      match t with
+      match o with
       | `Hold ->
-        let p1 = list1 b lst in
-        FHold (utof (on_province b p1))
+        let p1 = list1 t lst in
+        FHold (utof (on_province t p1))
       | `Move ->
-        let (p1,p2) = list2 b lst in
-        FMove (utof (on_province b p1), ptof p2)
+        let (p1,p2) = list2 t lst in
+        FMove (utof (on_province t p1), ptof p2)
       | `SupportD ->
-        let (p1,p2) = list2 b lst in
-        FSupportD (utof (on_province b p1), utof (on_province b p2))
+        let (p1,p2) = list2 t lst in
+        FSupportD (utof (on_province t p1), ptof p2)
       | `SupportA ->
-        let (p1,p2,p3) = list3 b lst in
-        FSupportA (utof (on_province b p1), utof (on_province b p2), ptof p3)
+        let (p1,p2,p3) = list3 t lst in
+        FSupportA (utof (on_province t p1), ptof p2, ptof p3)
       | `Convoy ->
-        let (p1,p2,p3) = list3 b lst in
-        FConvoy (utof (on_province b p1), ptoa p2, ptoa p3)
+        let (p1,p2,p3) = list3 t lst in
+        FConvoy (utof (on_province t p1), ptoa p2, ptoa p3)
     end
